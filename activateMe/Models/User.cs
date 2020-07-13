36036788b0acff0ec3fbcea7e0f799941e43a7ca
Charity Bunyon <CSEYMOU5@GMAIL.com>
2020-07-13 10:00:50 -0500

    added images, react setup, and vs setup

---
+++ activateMe/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace activateMe.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public DateTime dateJoined { get; set; }
+        public string userName { get; set; }
+        public string email { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string imageUrl { get; set; }
+        public  int points { get; set; }
+    }
+}
--- activateMe/Startup.cs
+++ activateMe/Startup.cs
@@ -5,17 +5,45 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace activateMe
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+                options.AddPolicy("ItsAllGood",
+                    builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin()));
+
+            var authSettings = Configuration.GetSection("AuthenticationSettings");
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                    {
+                        options.IncludeErrorDetails = true;
+                        options.Authority = authSettings["Authority"];
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuer = true,
+                            ValidIssuer = authSettings["Issuer"],
+                            ValidateAudience = true,
+                            ValidAudience = authSettings["Audience"],
+                            ValidateLifetime = true
+                        };
+                    }
+                );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
